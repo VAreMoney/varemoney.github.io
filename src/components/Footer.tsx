406caff656692350c50a4a027ac116ace34d453f
FlyingMongoose <flyingmongoose@gmail.com>
2024-10-02 02:29:18 -0400

    Fixed bottom footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,17 +3,11 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <section
-      id="buy"
-      className="min-h-screen flex items-center justify-center bg-black text-yellow-400"
-    >
-      <footer>
-        <hr />
-        <div className="footer-content">
-          Off<span>i</span>cial <a href="https://degenstudios.media" target="_blank" rel="noopener noreferrer">Degen Studios</a> Token
-        </div>
-      </footer>
-    </section>
+    <footer>
+      <div className="footer-content">
+        Off<span>i</span>cial <a href="https://degenstudios.media" target="_blank" rel="noopener noreferrer">Degen Studios</a> Token
+      </div>
+    </footer>
   );
 };
 
